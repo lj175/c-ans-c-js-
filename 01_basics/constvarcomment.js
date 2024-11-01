3443deadd8c130de39b1cd63294839494f397b3b
lj175 <144793201+lj175@users.noreply.github.com>
2024-11-01 12:42:47 +0000

    const let var done

---
+++ 01_basics/constvarcomment.js
@@ -0,0 +1,10 @@
+const accountid = 1223223
+let accountname = "lakshya"
+var accountage = "19"
+account_city = "bathinda" 
+let account_state
+console.table([accountid, accountname, accountage, account_city,account_state])
+
+//dont use var coz curly bracket and functional scope or soemthing is not good in js//
+//accountid=2 is not allowed coz const cant be changed //
+/* this method can also be used for commenting commenting is obv jo node read nhi krta to leave insights bw codes
